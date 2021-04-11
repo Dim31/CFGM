3bae4da565ddc96431819365a82371aa38051e5b
Valentin <parlier.valentin@gmail.com>
2021-04-11 16:59:05 +0200

    page compte : modif icon apple pay + background

--- src/Screens/Compte.js
+++ src/Screens/Compte.js
@@ -11,7 +11,7 @@ import {
     StatusBar
 } from 'react-native'
 import { ButtonGroup, Avatar, Divider } from "react-native-elements";
-import pattern2_travers from "../image/Patern2_travers.png";
+import backgroundImage from "../image/Patern2_travers2.png";
 import en_tete from "../image/en-tete.png";
 import call from "../image/call.png"
 import woman from "../image/woman.png"
@@ -28,125 +28,122 @@ import arrow from "../image/right-arrow.png"
 import Dash from "react-native-dash";
 
 class Compte extends React.Component {
-    state = {
-        index: 0
-    }
-    updateIndex = (index) => {
-        this.setState({index})
-    }
+  state = {
+    index: 0
+  }
+  updateIndex = (index) => {
+    this.setState({index})
+  }
 
-    render() {
-        const { selectedIndex } = this.state
-        return (
-            <SafeAreaView style={styles.main_container}>
-                <ImageBackground source={pattern2_travers} style={styles.patternStyle}>
-                    {/*En-tete*/}
-                    {/*
-                    <View style={styles.enTeteView}>
-                        <ImageBackground source={en_tete} style={styles.enTeteStyle}/>
-                    </View>
-                    */}
-                    <ScrollView style={styles.scrollView}>
-                        {/*Avatar*/}
-                        <View style={styles.avatarView}>
-                            <Avatar
-                                size={120}
-                                rounded
-                                source={{
-                                    uri:
-                                        'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-                                }}
-                            />
-                        </View>
-                        {/*Contenu*/}
-                        <View style={styles.contenuView}>
-                            {/*Informations*/}
-                            <View style={styles.informations}>
-                                <Text style={styles.title}>Informations:</Text>
-                                <View style={styles.bi_icons}>
-                                    <View style={styles.iconLeft}>
-                                        <Image source={call} style={styles.image}/>
-                                        {/*Numero en BDD*/}
-                                        <Text style={styles.text}>06.07.09.34.23</Text>
-                                    </View>
-                                    <View style={styles.iconRight}>
-                                        <Image source={woman} style={styles.image}/>
-                                        {/*Nombre de personnes dans le foyer en BDD*/}
-                                        <Text style={styles.text}>1 personne</Text>
-                                    </View>
-                                </View>
-                                <View style={styles.bi_icons}>
-                                    <View style={styles.iconLeft}>
-                                        <Image source={email} style={styles.image}/>
-                                        {/*Email en BDD*/}
-                                        <Text style={styles.text}>melissa.coste@gmail.com</Text>
-                                    </View>
-                                    <View style={styles.iconRight}>
-                                        <Image source={none} style={styles.image}/>
-                                        {/*Allergies en BDD*/}
-                                        <Text style={styles.text}>Aucune allergie</Text>
-                                    </View>
-                                </View>
-                                <View style={styles.bi_icons}>
-                                    <View style={styles.iconLeft}>
-                                        <Image source={localisation} style={styles.image}/>
-                                        {/*Adresse en BDD*/}
-                                        <Text style={styles.text}>12 rue de la poste {"\n"} 32000 Toulouse</Text>
-                                    </View>
-                                    <View style={styles.iconRight}>
-                                        <Image source={vege} style={styles.image}/>
-                                        {/*Régime en BDD*/}
-                                        <Text style={styles.text}>Végétarienne</Text>
-                                    </View>
-                                </View>
-                                <TouchableOpacity style={styles.modify}>
-                                    <Image source={pencil} style={styles.image}/>
-                                    <Text style={styles.text}>Modifier mes informations</Text>
-                                </TouchableOpacity>
-                                <Dash dashGap={10} dashLength={10} dashThickness={1} dashColor={'#F0955F'} style={{marginTop:20}}/>
-                            </View>
-                            {/*Mes commandes*/}
-                            <View style={styles.informations}>
-                                <Text style={styles.title}>Mes commandes:</Text>
-                                {/*TODO : Component à faire*/}
-                                <Dash dashGap={10} dashLength={10} dashThickness={1} dashColor={'#F0955F'} style={{marginTop:20}}/>
-                            </View>
-                            {/*Mes moyens de paiement*/}
-                            <View style={styles.informations}>
-                                <Text style={styles.title}>Mes moyens de paiement:</Text>
-                                <View style={styles.bi_icons}>
-                                    <TouchableOpacity style={styles.bi_icons}><Image source={paypal} style={styles.paymentLogo}/></TouchableOpacity>
-                                    <TouchableOpacity style={styles.bi_icons}><Image source={visa} style={styles.paymentLogo}/></TouchableOpacity>
-                                    <TouchableOpacity style={styles.bi_icons}><Image source={applepay} style={styles.paymentLogo}/></TouchableOpacity>
-                                    <TouchableOpacity style={styles.bi_icons}><Image source={mastercard} style={styles.paymentLogo}/></TouchableOpacity>
-                                </View>
-                            </View>
-                            {/*Coups de coeur*/}
-                            <View style={styles.informations}>
-                                <Text style={styles.title}>Coups de coeur:</Text>
-                                <Text>
-                                    {/*TODO : Component à faire*/}
-                                </Text>
-                            </View>
-                            {/*Parametres/Informations*/}
-                            <View style={styles.informations}>
-                                <TouchableOpacity style={styles.bi_icons}>
-                                    <Text style={styles.buttonParameter}>Paramètres</Text>
-                                    <Image source={arrow} style={styles.image}/>
-                                </TouchableOpacity>
-                                <Dash dashGap={0} dashLength={10} dashThickness={1} dashColor={'#707070'}/>
-                                <TouchableOpacity style={styles.bi_icons}>
-                                    <Text style={styles.buttonParameter}>Informations</Text>
-                                    <Image source={arrow} style={styles.image}/>
-                                </TouchableOpacity>
-                                <Dash dashGap={0} dashLength={10} dashThickness={1} dashColor={'#707070'}/>
-                            </View>
-                        </View>
-                    </ScrollView>
-                </ImageBackground>
-            </SafeAreaView>
-        )
-    }
+  render() {
+    const { selectedIndex } = this.state
+    return (
+      <ImageBackground source={backgroundImage} style={styles.backgroudImage}>
+        <SafeAreaView style={styles.main_container}>
+          {/*En-tete*/}
+          {/*
+          <View style={styles.enTeteView}>
+          <ImageBackground source={en_tete} style={styles.enTeteStyle}/>
+          </View>
+          */}
+          <ScrollView style={styles.scrollView}>
+            {/*Avatar*/}
+            <View style={styles.avatarView}>
+              <Avatar
+                size={120}
+                rounded
+                source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', }}
+              />
+            </View>
+            {/*Contenu*/}
+            <View style={styles.contenuView}>
+              {/*Informations*/}
+              <View style={styles.informations}>
+                <Text style={styles.title}>Informations:</Text>
+                <View style={styles.bi_icons}>
+                  <View style={styles.iconLeft}>
+                    <Image source={call} style={styles.image}/>
+                    {/*Numero en BDD*/}
+                    <Text style={styles.text}>06.07.09.34.23</Text>
+                  </View>
+                  <View style={styles.iconRight}>
+                    <Image source={woman} style={styles.image}/>
+                    {/*Nombre de personnes dans le foyer en BDD*/}
+                    <Text style={styles.text}>1 personne</Text>
+                  </View>
+                </View>
+                <View style={styles.bi_icons}>
+                  <View style={styles.iconLeft}>
+                    <Image source={email} style={styles.image}/>
+                    {/*Email en BDD*/}
+                    <Text style={styles.text}>melissa.coste@gmail.com</Text>
+                  </View>
+                  <View style={styles.iconRight}>
+                    <Image source={none} style={styles.image}/>
+                    {/*Allergies en BDD*/}
+                    <Text style={styles.text}>Aucune allergie</Text>
+                  </View>
+                </View>
+                <View style={styles.bi_icons}>
+                  <View style={styles.iconLeft}>
+                    <Image source={localisation} style={styles.image}/>
+                    {/*Adresse en BDD*/}
+                    <Text style={styles.text}>12 rue de la poste {"\n"} 32000 Toulouse</Text>
+                  </View>
+                  <View style={styles.iconRight}>
+                    <Image source={vege} style={styles.image}/>
+                    {/*Régime en BDD*/}
+                    <Text style={styles.text}>Végétarienne</Text>
+                  </View>
+                </View>
+                <TouchableOpacity style={styles.modify}>
+                  <Image source={pencil} style={styles.image}/>
+                  <Text style={styles.text}>Modifier mes informations</Text>
+                </TouchableOpacity>
+                <Dash dashGap={10} dashLength={10} dashThickness={1} dashColor={'#F0955F'} style={{marginTop:20}}/>
+              </View>
+              {/*Mes commandes*/}
+              <View style={styles.informations}>
+                <Text style={styles.title}>Mes commandes:</Text>
+                {/*TODO : Component à faire*/}
+                <Dash dashGap={10} dashLength={10} dashThickness={1} dashColor={'#F0955F'} style={{marginTop:20}}/>
+              </View>
+              {/*Mes moyens de paiement*/}
+              <View style={styles.informations}>
+                <Text style={styles.title}>Mes moyens de paiement:</Text>
+                <View style={styles.bi_icons}>
+                  <TouchableOpacity style={styles.bi_icons}><Image source={paypal} style={styles.paymentLogo}/></TouchableOpacity>
+                  <TouchableOpacity style={styles.bi_icons}><Image source={visa} style={styles.paymentLogo}/></TouchableOpacity>
+                  <TouchableOpacity style={styles.bi_icons}><Image source={applepay} style={styles.paymentLogo}/></TouchableOpacity>
+                  <TouchableOpacity style={styles.bi_icons}><Image source={mastercard} style={styles.paymentLogo}/></TouchableOpacity>
+                </View>
+              </View>
+              {/*Coups de coeur*/}
+              <View style={styles.informations}>
+                <Text style={styles.title}>Coups de coeur:</Text>
+                <Text>
+                  {/*TODO : Component à faire*/}
+                </Text>
+              </View>
+              {/*Parametres/Informations*/}
+              <View style={styles.informations}>
+                <TouchableOpacity style={styles.bi_icons}>
+                  <Text style={styles.buttonParameter}>Paramètres</Text>
+                  <Image source={arrow} style={styles.image}/>
+                </TouchableOpacity>
+                <Dash dashGap={0} dashLength={10} dashThickness={1} dashColor={'#707070'}/>
+                <TouchableOpacity style={styles.bi_icons}>
+                  <Text style={styles.buttonParameter}>Informations</Text>
+                  <Image source={arrow} style={styles.image}/>
+                </TouchableOpacity>
+                <Dash dashGap={0} dashLength={10} dashThickness={1} dashColor={'#707070'}/>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </ImageBackground>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -157,10 +154,10 @@ const styles = StyleSheet.create({
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
         paddingBottom: Platform.OS === 'android' ? 80 : 0,
     },
-    patternStyle: {
-        resizeMode: 'stretch',
-        height: '100%',
-        width: '100%',
+    backgroudImage: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "center",
     },
     enTeteStyle: {
         resizeMode: 'stretch',
